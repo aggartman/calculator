2c99e0a5ec96ec873294040cd63c64b18e2d5c71
Austin Gartman <aggartman@gmail.com>
2021-08-23 15:41:04 -0500

    Add working javascript functionality.

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+// Calculation Functions
 const add = function(a, b) {
     return +a + +b;
 }
@@ -5,13 +6,15 @@ const subtract = function(a, b) {
     return +a - +b;
 }
 const divide = function(a, b) {
-    const c = +a / +b;
-    return Math.round(c * 100) / 100;
+    if (b == '0') {
+        return 'ERR';
+    } else {
+        const c = +a / +b;
+        return Math.round(c * 100) / 100;
+    }
 }
-const multiply = function(values) {
-    const multi = (a, b) => a * b;
-    const total = values.reduce(multi);
-    return total;
+const multiply = function(a, b) {
+    return +a * +b;
 }
 const power = function(a, b) {
     const total = a ** b;
@@ -19,21 +22,85 @@ const power = function(a, b) {
 }
 const operate = function(a, operator, b) {
     if (operator == '+') {
-        add(a, b);
+        return add(a, b);
     } if (operator == '-') {
-        subtract(a, b);
+        return subtract(a, b);
     } if (operator == '/') {
-        divide(a, b);
+        return divide(a, b);
     } if (operator == '*') {
-        multiply(a , b);
+        return multiply(a , b);
     } if (operator == '**') {
-        power(a, b);
+        return power(a, b);
     }
 }
+
+// Clear calcTxt
 const clear = document.getElementById('c').addEventListener('click', function() {
     document.getElementById('calcTxt').innerText = ' ';
 });
 const clearE = document.getElementById('ce').addEventListener('click', function(){
     document.getElementById('calcTxt').innerText = " ";
 });
-const numbers = document.getElementsByClassName('nums')
+
+// Write numbers to calcTxt
+const numbers = document.getElementsByClassName('nums');
+let addTxt = function(){
+    if (document.getElementById('calcTxt').innerText !== 'ERR') {
+        let number = this.textContent;
+        document.getElementById('calcTxt').innerText += number;
+    } else {
+        document.getElementById('calcTxt').innerText = this.textContent;
+    }
+};
+for (var i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', addTxt)
+};
+
+// Store numbers 
+let x;
+let y; 
+let oper;
+const operators = document.getElementsByClassName('operator');
+let storeX = function() {
+    x = document.getElementById('calcTxt').innerText
+};
+let storeY = function() {
+    y = document.getElementById('calcTxt').innerText
+};
+
+// Operate 
+let operator = function() {
+    storeX();
+    oper = this.textContent;
+    document.getElementById('calcTxt').innerText = '';
+};
+for (var i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', operator)
+};
+let equal = function() {
+    if (x != null) {
+        storeY();
+        let result = operate(x, oper, y);
+        document.getElementById('calcTxt').innerText = result;
+    } else {
+        document.getElementById('calcTxt').innerText = 'ERR'
+    }
+};
+const equals = document.getElementById('equals').addEventListener('click', equal);
+
+// Percent 
+percentage = function() {
+    let num = document.getElementById('calcTxt').innerText
+    let per = (num / 100);
+    document.getElementById('calcTxt').innerText = per;
+};
+percent = document.getElementById('percent').addEventListener('click', percentage)
+
+// Decimal
+let decimals = function() {
+    let currentTxt = document.getElementById('calcTxt').innerText
+    if (!currentTxt.includes('.')) {
+        currentTxt += '.'
+    }
+}
+decimal = document.getElementById('decimal').addEventListener('click', decimals)
